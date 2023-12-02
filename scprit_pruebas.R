be224feedba3bb2870054a6f32f6e6f94fbc8a4e
JuliaArellano <jaa1018@alu.ubu.es>
2023-12-02 19:09:57 +0100

    temperatura tabla final

--- scprit_pruebas.R
+++ scprit_pruebas.R
@@ -19,13 +19,42 @@ temp_provincias <-  observaciones_diarias %>%
     tmedia = mean(tmed, na.rm = TRUE)
   )
 view(temp_provincias)
+
   # select(nombre, tmed) %>%
   #summarise(
     #tmed22 = mean(tmed, na.rm = TRUE)
   #)
-
-
-
+  temp_con_CCAA <- temp_provincias %>%
+  mutate(CCAA = case_when(
+    provincia %in% c("BURGOS", "AVILA", "LEON", "SALAMANCA", "SEGOVIA", "SORIA", "VALLADOLID" ,"PALENCIA" ,"ZAMORA") ~ "CASTILLA Y LEON",
+    provincia %in% c("ALBACETE", "CIUDAD REAL", "CUENCA", "GUADALAJARA", "TOLEDO") ~ "CASTILLA-LA MANCHA",
+    provincia %in% c("ALMERIA", "CADIZ", "CORDOBA", "GRANADA", "HUELVA", "JAEN", "MALAGA", "SEVILLA") ~ "ANDALUCIA",
+    provincia %in% c("HUESCA", "ZARAGOZA", "TERUEL") ~ "ARAGON",
+    provincia == "ASTURIAS" ~ "ASTURIAS",
+    provincia == "CANTABRIA" ~ "CANTABRIA",
+    provincia == "ILLES BALEARS" ~ "BALEARES",
+    provincia %in% c("STA. CRUZ DE TENERIFE", "LAS PALMAS") ~ "CANARIAS",
+    provincia == "LA RIOJA" ~ "LA RIOJA",
+    provincia %in% c("ARABA/ALAVA", "GIPUZKOA", "BIZKAIA") ~ "PAIS VASCO",
+    provincia %in% c("BARCELONA", "GIRONA", "LLEIDA", "TARRAGONA") ~ "CATALUÑA", 
+    provincia %in% c("ALICANTE", "CASTELLON", "VALENCIA") ~ "COMUNIDAD VALENCIANA",
+    provincia %in% c("BADAJOZ", "CACERES") ~ "EXTREMADURA",
+    provincia %in% c("A CORUÑA", "LUGO", "OURENSE", "PONTEVEDRA") ~ "GALICIA", 
+    provincia == "MADRID" ~ "COMUNIDAD DE MADRID",
+    provincia == "MURCIA" ~ "MURCIA",
+    provincia == "NAVARRA" ~ "NAVARRA",
+    provincia == "CEUTA" ~ "CEUTA",
+    provincia == "MELILLA" ~ "MELILLA",
+    
+  ))
+  view(temp_con_CCAA)
+ #TABLA FINAL 
+tmed_CCAA<- temp_con_CCAA %>% 
+  group_by(CCAA) %>% 
+  summarise(
+  tmedia = mean(tmedia, na.rm = TRUE)
+)
+  
 # Desde aquí ya tenemos nuestros datos de interés: tenemos q modificar en base a lo q ha dicho el profe
 
 # Temperatura 2021
